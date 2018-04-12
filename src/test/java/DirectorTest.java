068b24e3f84be678d54bdcb9b8d3309d39bfdcbb
Mike-Heneghan <m.heneghan@live.co.uk>
2018-04-12 16:34:49 +0100

    MVP finished and tested.

---
+++ src/test/java/DirectorTest.java
@@ -0,0 +1,51 @@
+import Staff.Management.Director;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DirectorTest {
+    private Director melinda;
+
+    @Before
+    public void before(){
+        melinda = new Director("Melinda", 8983938, 50000.00, "C.E.O", 500000.00);
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Melinda",melinda.getName());
+    }
+
+    @Test
+    public void hasNiNumber(){
+        assertEquals(8983938, melinda.getNiNumber());
+    }
+
+    @Test
+    public void hasSalary(){
+        assertEquals(50000.00, melinda.getSalary(), 0.001);
+    }
+
+    @Test
+    public void hasDeptName(){
+        assertEquals("C.E.O", melinda.getDeptName());
+    }
+
+    @Test
+    public void hasBudget(){
+        assertEquals(500000.00, melinda.getBudget(), 0.001);
+    }
+
+    @Test
+    public void canGetRaise(){
+        melinda.raiseSalary(5000.00);
+        assertEquals(55000, melinda.getSalary(), 0.001);
+    }
+
+    @Test
+    public void canGetBonus(){
+        assertEquals(500.00, melinda.payBonus(),0.001);
+    }
+
+}
