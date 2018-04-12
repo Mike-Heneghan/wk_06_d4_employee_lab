546a09f203231255fd5a927f4b51935cb129117f
Mike-Heneghan <m.heneghan@live.co.uk>
2018-04-12 15:15:51 +0100

    Initial commit with employee abstract class of staff package created.

---
+++ src/main/java/Staff/Employee.java
@@ -0,0 +1,34 @@
+package Staff;
+
+public abstract class Employee {
+    private String name;
+    private int niNumber;
+    private double salary;
+
+    public Employee(String name, int niNumber, double salary){
+        this.name = name;
+        this.niNumber = niNumber;
+        this.salary = salary;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getNiNumber(){
+        return niNumber;
+    }
+
+    public double getSalary(){
+        return salary;
+    }
+
+    public void raiseSalary(double raise){
+        salary += raise;
+    }
+
+    public double payBonus(){
+        return (salary * 0.01);
+    }
+
+}
